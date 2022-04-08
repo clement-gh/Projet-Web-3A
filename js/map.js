2e0534d45214fe6c5ee887dde80fe02a2565050c
clement-gh <64776760+clement-gh@users.noreply.github.com>
2022-04-08 07:22:26 +0200

    1

--- js/map.js
+++ js/map.js
@@ -1,15 +1,24 @@
 
+let test= "test";
 
 
-
-let fichier = "";
 const tab = [];
 
 document.addEventListener("DOMContentLoaded", () => {
   loadFichier();
+
 });
 
-function chargerTab() {
+function loadFichier() {
+
+
+    
+    console.log('fichier chragé')
+}
+
+
+
+function loadmap() {
   const grille = document.getElementById("grille");
   grille.innerHTML = '';
   for (let ligne of fichier.split(/\n/)) {
@@ -39,43 +48,41 @@ function chargerTab() {
     tab.push(tab1);
   }
   console.log(tab);
-  afficherTab();
+  printMap();
 }
 
-function loadFichier() {
-  
-}
 
-function afficherTab() {
+
+function printMap() {
   const grille = document.getElementById("grille");
   grille.innerHTML = '';
   for (let i = 0; i < tab.length; ++i) {
-    let tabHTML = '<div class="tab">';
+    let map = '<div class="tab">';
     for (let j = 0; j < tab[i].length; ++j) {
       if (tab[i][j] === "M") {
-        tabHTML += '<div class="mur"></div>';
+        map += '<div class="mur"></div>';
       }
       if (tab[i][j] === "T") {
-        tabHTML += '<div class="terre"></div>';
+        map += '<div class="terre"></div>';
       }
       if (tab[i][j] === "R") {
-        tabHTML += '<div class="rocher"></div>';
+        map += '<div class="rocher"></div>';
       }
       if (tab[i][j] === "D") {
-        tabHTML += '<div class="diamant"></div>';
+        map += '<div class="diamant"></div>';
       }
       if (tab[i][j] === "V") {
-        tabHTML += '<div class="vide"></div>';
+        map += '<div class="vide"></div>';
       }
       if (tab[i][j] === "P") {
-        tabHTML += '<div class="player"></div>';
+        map += '<div class="player"></div>';
       }
       else if (tab[i] === "0") {
-        tabHTML += '<div class="autre"></div>';
+        map += '<div class="autre"></div>';
       }
     }
-    tabHTML += '</div>';
-    grille.innerHTML += tabHTML;
+    map += '</div>';
+    grille.innerHTML += map;
   } 
 }
 
